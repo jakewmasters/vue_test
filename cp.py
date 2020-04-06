2b4c8ad9f1a9b63ea6cb73c8d072657e4610fe06
Jake Masters <jakewmasters@gmail.com>
2020-04-06 12:45:03 -0500

    initial commit

---
+++ cp.py
@@ -0,0 +1,11 @@
+# club penguin automated coin collector
+
+import pyautogui
+import time
+
+def main():
+    for i in range(5):
+        time.sleep(3)
+    return
+
+main()
---
+++ src/App.vue
@@ -0,0 +1,51 @@
+<template>
+  <div id="app">
+    <!-- CSS grid -->
+    <div class="wrapper">
+      <div class="box">A</div>
+      <div class="box">B</div>
+      <div class="box">C</div>
+      <div class="box">D</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  components: {
+  }
+}
+</script>
+
+<style>
+#app {
+  -webkit-font-smoothing: antialiased;
+}
+
+.wrapper {
+  display: grid;
+  grid-template-columns: 100px 100px 100px;
+  grid-gap: 10px;
+  background-color: #fff;
+  color: #444;
+}
+
+.box {
+  background-color: #444;
+  color: #fff;
+  border-radius: 5px;
+  padding: 20px;
+  font-size: 150%;
+}
+
+/* original CSS for reference */
+/* #app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+} */
+</style>
